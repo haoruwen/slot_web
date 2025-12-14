f91c4fc74ccc44f5583163d99d25bb9e9f9b4291
haoruwen <147346355+haoruwen@users.noreply.github.com>
2025-12-13 20:57:58 -0800

    adjust main avatar size for draw_others, updated talent records

--- app/services/administrator.py
+++ app/services/administrator.py
@@ -26,12 +26,27 @@ class Administrator:
             record = "%s drew a %s-tier %s for %s at %s" % (row['from_name'],row['tier'],row['prize_name'],row['to_name'],time_to_string(row['create_time']))
             records.append(record)
         return records
+
+    def _format_time(self, dt):
+        today = datetime.now().date()
+        if dt.date() == today:
+            return dt.strftime("%I:%M %p").lstrip("0")  # 今天只显示时间，去掉前导零
+        else:
+            return dt.strftime("%b %d, %I:%M %p").lstrip("0")  # 非今天显示月日+时间
     
     def get_user_records(self, user_name):
-        sql = "SELECT r.id, r.from_id,fu.name AS from_name,r.to_id,tu.name AS to_name,r.prize_id,p.name AS prize_name,r.tier,r.create_time FROM tbl_records r JOIN tbl_users fu ON r.from_id = fu.id JOIN tbl_users tu ON r.to_id = tu.id JOIN tbl_prize p ON r.prize_id = p.id where tu.name = %s ORDER BY r.create_time DESC"
-        rows = self.db_proxy.run_sql_select(sql, params=(user_name,))
+        sql = "SELECT r.id, r.from_id,fu.name AS from_name,r.to_id,tu.name AS to_name,r.prize_id,p.name AS prize_name,r.tier,r.create_time FROM tbl_records r JOIN tbl_users fu ON r.from_id = fu.id JOIN tbl_users tu ON r.to_id = tu.id JOIN tbl_prize p ON r.prize_id = p.id where tu.name = %s or fu.name = %s ORDER BY r.create_time DESC"
+        rows = self.db_proxy.run_sql_select(sql, params=(user_name, user_name))
         records = []
         for row in rows:
-            record = "%s received a %s-tier %s from %s at %s" % (row['to_name'],row['tier'],row['prize_name'],row['from_name'],time_to_string(row['create_time']))
-            records.append(record)
+            time_str = self._format_time(row['create_time'])
+            if row['to_name'] == row['from_name']:
+                record = "✨ You drew a %s-tier %s for yourself at %s" % (row['tier'], row['prize_name'], time_str)
+                records.append(record)
+            elif row['from_name'] == user_name:
+                record = "🎁 You drew a %s-tier %s for %s at %s" % (row['tier'], row['prize_name'], row['to_name'],time_str )
+                records.append(record)
+            else:
+                record = "🎉 You recived a %s-tier %s from %s at %s" % (row['tier'],row['prize_name'],row['from_name'], time_str)
+                records.append(record)
         return records
