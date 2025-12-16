278adc5d6eff18dc77d4289c41da03eed15b4bbd
haoruwen <147346355+haoruwen@users.noreply.github.com>
2025-12-15 23:10:11 -0800

    added forbidden prizes

--- utils/config.py
+++ utils/config.py
@@ -6,9 +6,9 @@ DB_NAME = "db_slot"
 IP_BLOCK_TIMES = 10
 TIERS = ['first', 'second', 'third']
 
-FIRST_TIER_POINTS = 10
-SECOND_TIER_POINTS = 5
-THIRD_TIER_POINTS = 1
+FIRST_TIER_POINTS = 600
+SECOND_TIER_POINTS = 250
+THIRD_TIER_POINTS = 100
 
 FIRST_TIER_BONUS_POINTS = 3
 SECOND_TIER_BONUS_POINTS = 2
@@ -20,4 +20,4 @@ POINTS_ERROR_MESSAGE = "Your points aren't enough to join this round."
 PRIZE_ERROR_MESSAGE = "All prizes in this tier have been claimed."
 
 TALENTS = ["Hina","Memory"]
-T1_REWARDS = ["Dakimakura_Design", "Laptop", "Staff_Choice_Newoutfit", "1_Key_Illustraion_From_Your_Artist", "5070_GPU"]
+T1_REWARDS = ["Dakimakura_Design", "Laptop", "Staff_Choice_Newoutfit", "1_Key_Illustration_From_Your_Artist", "5070_GPU"]
